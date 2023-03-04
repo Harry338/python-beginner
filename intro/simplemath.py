6626c27e41f22f4e23c81027d10a2d72afdb2aff
Harry Moon <harry.moon338@gmail.com>
2023-03-04 15:16:32 +1100

    updated simplemath.py

---
+++ intro/simplemath.py
@@ -0,0 +1,17 @@
+# print 2 + 2
+print(2 + 2)
+
+# "" = string
+# string is a bunch of characters that
+# cannot be calculated numerically
+print("2 + 2")
+
+# example of string and integer 
+# throwing an error
+# print("1" + 1)
+
+# multiplication - asterix
+print(3*3)
+
+# division - slash
+print(9/3)
