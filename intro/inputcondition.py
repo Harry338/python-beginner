8fc897206b5ca5b193e4da60a71a995acc3e7dbf
Harry Moon <harry.moon338@gmail.com>
2023-03-18 15:21:20 +1100

    created inputcondition.py

---
+++ intro/inputcondition.py
@@ -0,0 +1,13 @@
+user_reply = input("Do you like robots? (Type yes or no)")
+
+if user_reply == "yes" :
+    print("Beep Boop!")
+elif user_reply == "no":
+    print("Well, robots don't like you either")
+elif user_reply == "maybe":
+    print("Make up your mind, human.")
+elif user_reply == "only ones with laser eyes":
+    print("Zzzap!")
+else:
+    print("Please, your human nonsense offends us")
+    
