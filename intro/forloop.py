c3ca075ed3aa8d89af076f69015fd5fcd11806aa
Harry Moon <harry.moon338@gmail.com>
2023-04-29 15:11:46 +1000

    finished forloop.py

---
+++ intro/forloop.py
@@ -0,0 +1,10 @@
+
+# x = 0
+# while x < 10:
+#     print(x)
+#     x = x + 1
+
+# for loop which counts every number in the range (0,10)
+# print for every number 'y' is at 
+for y in range(0,10):
+    print(y)
