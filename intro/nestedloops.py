9ef8ace532f02994be2f74c1088d73d84623ae02
Harry Moon <harry.moon338@gmail.com>
2023-05-06 14:48:19 +1000

    finished nestedloops.py

--- intro/nestedloops.py
+++ intro/nestedloops.py
@@ -8,5 +8,19 @@
 
 # maths times table
 
+# range(0,13) = [1,2,3,4,5,6,7,8,9,10,11,12]
+
 for y in range(0,13):
     print("7 x ", y, " = ", 7*y)
+
+# counter = 0
+# while (counter < 13)
+# print("7 x ", y, " = ", 7*y)
+# counter = counter + 1
+
+
+# useful for other languages
+# for (counter = 0; counter < 13; counter+=1):
+#     print("7 x ", y, " = ", 7*y)
+
+
