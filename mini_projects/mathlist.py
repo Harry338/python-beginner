94ce86a09f700534920a07abde4f874d749d59ca
Harry Moon <harry.moon338@gmail.com>
2023-05-13 15:19:15 +1000

    edited and made mini_projects and mathlist.py

---
+++ mini_projects/mathlist.py
@@ -0,0 +1,30 @@
+# create a list with numbers ranging from 1 to 5
+# and call it "num_list"
+num_list = [1, 2, 3, 4, 5]
+
+# create another list with 5 consecutive odd numbers
+# and call it "odd_list"
+odd_list = [1, 3, 5, 7, 9]
+
+# manually prints the difference of the first item in
+# both lists
+# this is a clue
+
+# for loop to print the difference of every item in 
+# both lists
+
+# this is for subtracting
+# for i in range(0,5):
+#     print(odd_list[i] - num_list[i])
+
+# # this is for adding
+# for i in range(0,5):
+#     print(odd_list[i] + num_list[i])
+
+# # this is for multiplying
+# for i in range(0,5):
+#     print(odd_list[i] * num_list[i])
+
+# # this is for division
+# for i in range(0,5):
+#     print(odd_list[i] / num_list[i])
