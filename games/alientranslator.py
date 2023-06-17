4af5258b0f44bb7a8e10738074c6112e8cc22924
Harry Moon <harry.moon338@gmail.com>
2023-06-17 15:29:10 +1000

    alientranslator.py

---
+++ games/alientranslator.py
@@ -0,0 +1,28 @@
+alien_dictionary = {
+    "we":"vorag", 
+    "come":"thang", 
+    "in":"zon", 
+    "peace":"argh", 
+    "hello":"kodar", 
+    "can":"znak", 
+    "i":"az", 
+    "borrow":"liftit", 
+    "some":"zum", 
+    "rocket":"upgoman", 
+    "fuel":"kakboom", 
+    "please":"selpin", 
+    "don't":"baaaaaaaaaaarn", 
+    "shoot":"flabil", 
+    "welcome":"unkip", 
+    "our":"mandig", 
+    "new":"brang", 
+    "alien":"marangin", 
+    "overlords":"bap"
+    }
+
+english= input("Please enter an English word or phrase to translate: \n")
+english.lower().split()
+
+for word in english:
+    print(word)
+
--- games/guessing.py
+++ games/guessing.py
@@ -1,9 +1,11 @@
 import random
 number = random.randint(1,20)
 guess = -1
+
 while guess != number:
     guess = int(input("I'm thinking of a number between 1 and 20. What is it?"))
     
+    # if statements to check if guess is correct
     if guess == number:
         print("Nice! You guessed correctly")
     elif guess < number:
