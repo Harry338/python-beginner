58784c067bcd5141a0998f9a9b70e5e28472b269
Harry Moon <harry.moon338@gmail.com>
2023-07-29 15:27:50 +1000

    finished spymessages.py

--- games/spymessages.py
+++ games/spymessages.py
@@ -3,26 +3,31 @@
 
 alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
-string_to_encrypt = input("PLease enter a message to encrypt: \n")
+string_to_encrypt = input("Please enter a message to encrypt:\n")
 
 # turns all the letters in the variable to upper case
 string_to_encrypt = string_to_encrypt.upper()
 
-shift_amount = int(input("Please enter a whole number from 1-25 to be your shift key.  "))
+shift_amount = int(input("Please enter a whole number from 1-25 to be your shift key. "))
 
 encrypted_string = ""
 
 for letter in string_to_encrypt:
-    # first get the position of the letter in the alphabet
-    # using .find(letter)
     position = alphabet.find(letter)
-    
-    new_position = ((shift_amount) + (position))
+    new_position = position + shift_amount
+    if letter in alphabet:
+        encrypted_string = encrypted_string + alphabet[new_position]
+    else:
+        encrypted_string = encrypted_string + letter
+
+    print("Your encrypted message is", encrypted_string)
+
+
+# first get the position of the letter in the alphabet
+    # using .find(letter)
 
     # make a new_position value which is the 
     # sum of the shift_amount and the found position amount
 
     # for now, print the letter in the alphabet with the new_position 
     # using your knowledge that strings are technically lists
-    
-    print(letter)
---
+++ games/turtledrawing.py
@@ -0,0 +1,15 @@
+from turtle import *
+
+color("limegreen")
+shape("turtle")
+speed(1)
+pensize(5)
+
+forward(50)
+right(90)
+forward(50)
+right(90)
+forward(50)
+right(90)
+forward(50)
+
