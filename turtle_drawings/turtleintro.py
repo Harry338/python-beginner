dfab19110208101fc3a8bfe165abec5e755b2090
Harry Moon <harry.moon338@gmail.com>
2023-08-05 15:19:07 +1000

    finished turtledrawing.py

---
+++ turtle_drawings/turtleintro.py
@@ -0,0 +1,33 @@
+# imports the turtle file, but have to use turtle.function_name
+# import turtle
+
+# imports every function in the turtle file
+from turtle import *
+
+# imports:
+# is basically getting a library from the python that you downloaded
+# in the beginning
+
+# defines color 
+color("limegreen")
+
+# defines shape of the drawing
+shape("turtle")
+
+# defines the speed of the drawing
+speed(2)
+
+# defines the thickness of the line that will be drawn
+pensize(5)
+
+# Drawing a square:
+# define how long the forward line is and drawn it
+forward(50)
+right(90)
+forward(50)
+right(90)
+forward(50)
+right(90)
+forward(50)
+
+done()
